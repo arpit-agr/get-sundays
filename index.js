6c55aee08328ae98939c0b125c758fb3d302a59e
arpit-agr <arpit.agrawal2502@gmail.com>
2026-09-25 03:16:42 +0530

    Step through Sundays 7 days at a time in both branches

--- index.js
+++ index.js
@@ -26,36 +26,64 @@ export function getSundays(year, month) {
 
 		const numberOfDays = plainDateObj.daysInMonth;
 
-		for (let day = 1; day <= numberOfDays; day++) {
+		let firstSunday;
+
+		for (let dayOfMonth = 1; dayOfMonth <= 7; dayOfMonth++) {
 			const dateObj = Temporal.PlainDate.from({
 				year: year,
 				month: month,
-				day: day,
+				day: dayOfMonth,
 			});
 			const dayOfWeek = dateObj.dayOfWeek;
-			const isSunday = dayOfWeek % 7 === 0;
+			const isSunday = dayOfWeek === 7;
 
 			if (isSunday) {
-				const dateString = dateObj.toString();
-				sundaysArr.push(dateString);
+				firstSunday = dayOfMonth;
+				break;
 			}
+		}
+
+		for (
+			let dayOfMonth = firstSunday;
+			dayOfMonth <= numberOfDays;
+			dayOfMonth = dayOfMonth + 7
+		) {
+			const dateObj = Temporal.PlainDate.from({
+				year: year,
+				month: month,
+				day: dayOfMonth,
+			});
+			const dateString = dateObj.toString();
+			sundaysArr.push(dateString);
 		}
 	} else {
 		const date = new Date(0);
 		date.setUTCFullYear(year, month, 0);
 		const numberOfDays = date.getUTCDate();
+		let firstSunday;
 
-		for (let day = 1; day <= numberOfDays; day++) {
+		for (let dayOfMonth = 1; dayOfMonth <= 7; dayOfMonth++) {
 			const date = new Date(0);
-			date.setUTCFullYear(year, month - 1, day);
+			date.setUTCFullYear(year, month - 1, dayOfMonth);
 			const dayOfWeek = date.getUTCDay();
 			const isSunday = dayOfWeek === 0;
 
 			if (isSunday) {
-				const dateString = date.toISOString();
-				const formattedDateString = dateString.split("T")[0];
-				sundaysArr.push(formattedDateString);
+				firstSunday = dayOfMonth;
+				break;
 			}
+		}
+
+		for (
+			let dayOfMonth = firstSunday;
+			dayOfMonth <= numberOfDays;
+			dayOfMonth = dayOfMonth + 7
+		) {
+			const date = new Date(0);
+			date.setUTCFullYear(year, month - 1, dayOfMonth);
+			const dateString = date.toISOString();
+			const formattedDateString = dateString.split("T")[0];
+			sundaysArr.push(formattedDateString);
 		}
 	}
 
